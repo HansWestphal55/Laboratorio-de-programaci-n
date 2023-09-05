b7e75997b122901a73bc4d67a200c0d9a43d814e
HansWestphal55 <144158151+HansWestphal55@users.noreply.github.com>
2023-09-05 12:27:55 -0600

    Add files via upload

---
+++ L1 HEWM_1135223.py
@@ -0,0 +1,15 @@
+#Hans Westphal 1135223 Seccion 17 5/09/2023
+#Asigmar variables y operarlas. 
+print("Operaciones con variables N1 y N2")
+N1 = 0
+N2 = 0
+N1 = input("Ingrsar numero 1")
+N2 = input("Ingresar numero 2")
+suma = N1+N2
+resta = N2-N1
+multiplicacion = N1*N2
+division = N2/N1
+print(suma)
+print (resta)
+print(multiplicacion)
+print (division)
