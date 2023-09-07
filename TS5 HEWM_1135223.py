7fbb1ed10162da332752732e7f4b3b1ddfe64353
HansWestphal55 <144158151+HansWestphal55@users.noreply.github.com>
2023-09-07 09:16:20 -0600

    Add files via upload

---
+++ TS5 HEWM_1135223.py
@@ -0,0 +1,13 @@
+#Hans Westphal 1135223
+#trabajo supervisado 17
+#7/9/2023
+#Entrada
+sNombre = input("ingresar nombre")
+sEdad = input ("ingresar edad")
+sCarrera = input("Ingresar carrera")
+#Objetivo y proceso principal
+print("nombre", sNombre)
+print("edad", sEdad)
+print("carrera", sCarrera)
+#Salida 
+print("soy", sNombre,"tengo", sEdad, " años, estudio", sCarrera, ",este es mi segundo programa.")
