40758b4d64c910e13f016a8fd62a53550398ddea
HansWestphal55 <144158151+HansWestphal55@users.noreply.github.com>
2023-09-21 09:41:01 -0600

    Add files via upload

---
+++ TS7-HEWM-1135223.py
@@ -0,0 +1,16 @@
+#Trabajo supervisado de programación 
+#Hans Westphal 1135223
+#21/9/23
+#Multiplicación de números en una lista 
+#Nombre
+print("mi nombre es: Hans Ernst Westphal Menéndez")
+x=""
+while x == "salida": 
+    n = int(input("ingrese un número"))
+    for i in range(1,11):
+        resultado = n*i
+        print(resultado)
+    x=input("ingresar la palabra salida")
+    break
+
+
